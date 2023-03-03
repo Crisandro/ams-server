9d018228db2250cef9d1a7d04a46c33a3b1f934e
Crisandro Basoy <crisandro.cloudemployee@gmail.com>
2023-03-03 08:54:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(session({
 
   // Add Access Control Allow Origin headers
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
